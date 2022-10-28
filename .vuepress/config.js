d5d1acc847c8b73ebfdfd8deb5bc65b8f25613b8
jongminshin <ace4463@gmail.com>
2022-10-28 18:04:16 +0900

    vuepress init

---
+++ .vuepress/config.js
@@ -0,0 +1,16 @@
+// config.js
+module.exports = {
+  title: '개발자 신종민',
+  description: 'Today I Learned',
+  themeConfig: {
+    logo: 'https://avatars.githubusercontent.com/u/53161335?v=4',
+    nav: [
+      { text: 'Home', link: '/' },
+      {text: 'About', link: '/About/'},
+    ],
+    lastUpdated: true,
+    sidebar: 'auto',
+    smoothScroll: true
+  },
+  base: '/TIL/',
+}
